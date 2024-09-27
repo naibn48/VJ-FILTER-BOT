630121b8dc6ee46832a23e65d55fb68cec158cdc
naibn48 <158262557+naibn48@users.noreply.github.com>
2024-09-27 15:50:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -66,7 +66,7 @@ PAYMENT_TEXT = environ.get('PAYMENT_TEXT', '<b>- ᴀᴠᴀɪʟᴀʙʟᴇ ᴘʟ�
 OWNER_USERNAME = environ.get('OWNER_USERNAME', 'MSxSupportxBot') # owner username without @
 
 # Clone Information : If Clone Mode Is True Then Bot Clone Other Bots.
-CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
+CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
 CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "mongodb+srv://Naibn48:Shakeer@123@cluster0.udkbga9.mongodb.net/?retryWrites=true&w=majority") # Necessary If clone mode is true
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'MS_Movies_Request') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
