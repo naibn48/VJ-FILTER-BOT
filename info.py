a0b53595a98c6e549030358d2b334bb853e605b6
naibn48 <158262557+naibn48@users.noreply.github.com>
2024-10-03 18:15:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -73,7 +73,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+FkmEFX51IbxkNmM1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+aoQexyyka_A1MzM1')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_This_Linkz')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_This_Linkz/2')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'MSxSupportxBot') # Support Chat Link Without https:// or @
 
 # True Or False
